4d0490c698a45324e41c27ccffe77afb549051de
joe-boadi <joeboadi1@gmail.com>
2024-10-04 19:18:46 +0000

    Initial setup and commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,58 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+// Interface for the project structure.
+interface Product {
+  id: number;
+  title: string;
+  images: string[];
+  price: number;
+  description: string;
+  category: string;
+  rating: number;
+  reviews: number;
+}
+
+function App() {
+
+  const [ products, setProducts ] = useState<Product[]>([])
+  const [ loading, setLoading ] = useState(true);
+  const [error, setError ] = useState<string | null>(null);
+
+  useEffect(() => {
+
+    // https://dummyjson.com/docs/products
+    // ./database/product.json
+
+    axios.get<{products: Product[]}>('https://dummyjson.com/docs/products')
+    .then((res) => {
+      setProducts(res.data.products);
+      console.log(setProducts)
+    }).catch( err => { 
+        console.error(err)
+        setLoading(false)
+        setError("Failed to fetch products")
+      });
+  }, [])
+
+  if(loading) return <div>Loading</div>
+  if(error) return <div>Error: {error}</div>
+
+  return (
+    <>
+      <div>
+        {products.map((product, index) => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <img src={product.images[index]} alt={product.title} />
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
