532402794dcefead49045cd23453eabacc5b01bc
joe-boadi <joeboadi1@gmail.com>
2024-10-07 12:02:10 +0000

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,12 @@
-
-import { useEffect, useState } from 'react'
 import './App.css'
-import axios from 'axios'
+import FetchAPI from './utils/FetchAPI'
 
-// Interface for the data structure.
-interface Product {
-  id: number;
-  title: string;
-  images: string[];
-  price: number;
-  description: string;
-  category: string;
-  rating: number;
-  reviews: number;
-}
-
-function App() {
-
-  const [ products, setProducts ] = useState<Product[]>([])
-  const [ loading, setLoading ] = useState(true);
-  const [error, setError ] = useState<string | null>(null);
-
-  useEffect(() => {
-    axios.get<{products: Product[]}>('https://dummyjson.com/docs/products')
-    .then((res) => {
-      setProducts(res.data.products);
-
-      // Verify data is valid
-      console.log(setProducts.length, res.data.products);
-    
-    }).catch( err => { 
-        console.error(err)
-        setLoading(false)
-        setError("Failed to fetch products") // TODO: FIXME: error message (Rectify data fetch)
-      });
-  }, [])
-
-  if(loading) return <div>Loading</div>
-  if(error) return <div>Error: {error}</div>
+function App(){
 
   return (
-      <div>
-        {products.length > 0 ? (products.map( (product, index) => (
-          <div key={index}>
-            <h2>{product.title}</h2>
-            <img src={product.images[index]} alt={product.title} />
-            <p>{product.price}</p>
-          </div>)
-        )) : (
-          <div>No products found...</div>
-        )}
-      </div>
+    <>
+      <FetchAPI />
+    </>
   )
 }
 
