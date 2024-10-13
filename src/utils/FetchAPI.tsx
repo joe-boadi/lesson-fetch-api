b80a55598e0a52a610cb33fb2fcd0105a0acf9f5
joe-boadi <joeboadi1@gmail.com>
2024-10-13 14:55:19 +0000

    feat: Add Suspense Loading and skeleton

--- src/utils/FetchAPI.tsx
+++ src/utils/FetchAPI.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { Suspense, useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   Card,
   CardContent,
-  CardDescription,
+  // CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-// import style from '@/styles/fetchAPI.module.css'
+} from "@/components/ui/card";
+import UseLoadingStateSkeleton from '@/components/hooks/Loading';
+import UseErrorLoading  from '@/components/hooks/ErrorLoading';
 
 // Interface for the data structure.
 interface Product {
@@ -20,58 +21,58 @@ interface Product {
   category: string;
   rating: number;
   reviews: number;
-}
+};
 
 function Products () {
-  const [ products, setProducts ] = useState<Product[]>([])
+  const [ products, setProducts ] = useState<Product[]>([]);
   const [ loading, setLoading ] = useState(true);
   const [ error, setError ] = useState<string | null>(null);
 
   const fetchApiResponse = async () => {
     try {
-      // ./database/product
+      // ./database/products
       // https://dummyjson.com/products
       const res = await axios.get('https://dummyjson.com/products');
         setProducts(res.data.products);
         console.log(res.data.products);
-
     } catch (err) { 
           console.error(err)
           setError("Failed to fetch products")
-
       } finally {
-          setLoading(false)
+          setLoading(false);
         };
-    }
+  }
 
-  fetchApiResponse();
-  
-  if(loading) return <div>Loading...</div>;
-  if(error) return <div>Error fetching data</div>
+  useEffect(() =>{
+    fetchApiResponse(); // This useEffect will run only once when the component mounts and will clean up when the component unmounts.
+  },[])
+
+  if(loading) return < UseLoadingStateSkeleton />;
+  if(error) return < UseErrorLoading />;
 
   return (
-    <div className=''>
+    <>
       {products.map((product, index) => (
-       <Card key={product.id} className='grid grid-cols-4 gap-4 p-4'>
-          <CardHeader>
-            <img src={product.images[0]} alt={product.title} />
-            <CardTitle>{product.title}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <CardDescription>{product.description}</CardDescription>
-            <p>Price: ${product.price}</p>
-            <p>Rating: {product.rating}</p>
-          </CardContent>
-          <CardFooter>
-            <p>Reviews: {product.reviews}</p>
-          </CardFooter>
-          {/*Add a horizontal rule every second product card for better readability.*/}
-          {index % 2 === 0 && <hr />}         
-          {/* Add other styles or features as needed */}
-       </Card>
+        <Suspense key={product.id} fallback={loading}>
+          <Card className='grid grid-cols-4 gap-4 p-4'>
+            <CardHeader>
+              <img src={product.images[0]} alt={product.title} width={210}/>
+              <CardTitle>{product.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              {/* <CardDescription>{product.description}</CardDescription> */}
+              <p>Price: ${product.price.toFixed(2)}</p>
+              <p>Rating: {product.rating}</p>
+            </CardContent>
+            {/* You can include the id if you want it displayed */}
+            <CardFooter>
+              <p>Product ID: {product.id}</p>
+            </CardFooter>
+            {index % 2 === 0 && <hr />}
+          </Card>
+        </Suspense>
       ))}
-    </div>
+  </>
   )
 };
-
-export default Products
+export default Products;
