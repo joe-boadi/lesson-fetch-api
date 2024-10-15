c9381c271b9b9eddec28d8612847fe330f6b08c7
joe-boadi <joeboadi1@gmail.com>
2024-10-15 20:24:02 +0000

    Add footer and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 import FetchAPI from './utils/FetchAPI'
 
 function App(){
 
   return (
     <>
+      <Navbar />
       <FetchAPI />
+      <Footer />
     </>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,21 @@
-import { buttonVariants } from "@/components/ui/button"
-
-
+// import { buttonVariants } from "@/components/ui/button"
+import styles from '@/styles/Navbar.module.css';
+import Button from '@/styles/Button.module.css';
+import { FaShopify } from "react-icons/fa";
+import { IoLogInSharp } from 'react-icons/io5';
+import { FaCartShopping } from 'react-icons/fa6';
 
 export default function Navbar () {
     return (
-        <nav className="justify-between p-4 sticky blur-lg m-2">
+        <nav className={`${styles.Navbar}`}>
             <div>
-                <ul className=" text-lg">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                <ul className={`${styles.Navbar}`}>
+                    <li><a href="/"> <FaShopify className={`${styles.HomeIcon}`}/> </a></li>
+                    <li><a href="/cart"><FaCartShopping className={`${styles.cartIcon}`}/></a></li>
                 </ul>
             </div>
-
-            <button className={buttonVariants({ variant: "outline" })}>Log in</button>
-
+            {/* buttonVariants({ variant: "outline" }) */}
+            <button name='Login' className={`${Button.button}`}><IoLogInSharp />Login</button> 
         </nav>
     )
-}
+};
